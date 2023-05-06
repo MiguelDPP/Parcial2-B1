90d43f5ccc41eeeff35360bfc73210e7ce73e59b
HwarolS <harolfsanchez25@gmail.com>
2023-05-05 20:51:53 -0500

    se arregló lo de los desayunos, almuerzos y cenas

--- src/app/pages/restaurant/restaurant.page.ts
+++ src/app/pages/restaurant/restaurant.page.ts
@@ -25,7 +25,7 @@ export class RestaurantPage implements OnInit {
     {
       id: '1',
       name: 'Tostadas francesas con frutas frescas y miel',
-      price: '$ 3.0000',
+      price: '$ 3.000',
       img: '/assets/img/desayunos/1.jfif',
       active: false,
       type: '1',
@@ -53,15 +53,47 @@ export class RestaurantPage implements OnInit {
     img: '/assets/img/desayunos/4.jpg',
     active: false,
     type: '1',
-  },
+    },
+    {
+    id: '5',
+  name: 'Empanadas rellenas de carne y acompañadas de suero costeño',
+  price: '$ 2.000',
+  img: '/assets/img/desayunos/5.jpg',
+  active: false,
+  type: '1',
+},
   ];
 
   almuerzos = [
     {
-      id: '1',
+      id: '6',
       name: 'Ensalada César con pollo a la parrilla',
       price: '$ 5.000',
       img: '/assets/img/almuerzos/1-1.jpg',
+      active: false,
+      type: '2',
+    },
+    {
+      id: '7',
+      name: 'Bandeja paisa',
+      price: '$ 9.000',
+      img: '/assets/img/almuerzos/1-2.gif',
+      active: false,
+      type: '2',
+    },
+    {
+      id: '8',
+      name: 'Pollo sudado',
+      price: '$ 8.000',
+      img: '/assets/img/almuerzos/1-3.jpg',
+      active: false,
+      type: '2',
+    },
+    {
+      id: '9',
+      name: 'Sopa de mondongo',
+      price: '$ 4.000',
+      img: '/assets/img/almuerzos/1-4.jpg',
       active: false,
       type: '2',
     },
@@ -69,10 +101,34 @@ export class RestaurantPage implements OnInit {
 
   cenas = [
     {
-      id: '1',
-      name: 'Tostadas francesas con frutas frescas y miel',
-      price: '$ 3.0000',
-      img: '/assets/img/desayunos/1.jfif',
+      id: '10',
+      name: 'Arepa de huevo',
+      price: '$ 2.000',
+      img: '/assets/img/cenas/2-1.jpg',
+      active: false,
+      type: '3',
+    },
+    {
+      id: '11',
+      name: 'Tamal',
+      price: '$ 2.500',
+      img: '/assets/img/cenas/2-2.jpeg',
+      active: false,
+      type: '3',
+    },
+    {
+      id: '12',
+      name: 'Carimañolas',
+      price: '$ 2.000',
+      img: '/assets/img/cenas/2-3.jpg',
+      active: false,
+      type: '3',
+    },
+    {
+      id: '13',
+      name: 'Chorizo con arepa',
+      price: '$ 2.500',
+      img: '/assets/img/cenas/2-4.jpg',
       active: false,
       type: '3',
     },
@@ -80,6 +136,14 @@ export class RestaurantPage implements OnInit {
 
   clearFood() {
     this.desayunos.forEach((item) => {
+      item.active = false;
+    }
+    );
+    this.almuerzos.forEach((item) => {
+      item.active = false;
+    }
+    );
+    this.cenas.forEach((item) => {
       item.active = false;
     }
     );
@@ -152,12 +216,29 @@ export class RestaurantPage implements OnInit {
         this.desayunos.forEach((item2) => {
           if (item2.id === item.id) {
             item2.active = true;
+            this.current = item2;
           }
-
-          this.current = item2;
         }
         );
         break;
+        case '2':
+          this.almuerzos.forEach((item2) => {
+            if (item2.id === item.id) {
+              item2.active = true;
+              this.current = item2;
+            }
+          }
+          );
+        break;
+        case '3':
+          this.cenas.forEach((item2) => {
+            if (item2.id === item.id) {
+              item2.active = true;
+              this.current = item2;
+            }
+          }
+          );
+          break;
     }
   }
 
